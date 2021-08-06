464481eb3d46ea647bfb8bcae8a972a4ee926746
Fathyelshemy8 <fathyelshemy8@gmail.com>
2021-08-07 00:09:44 +0200

    Initial commit with skeleton of spring project

---
+++ src/main/java/com/blueharvest/bank/BlueHarvestBankApplication.java
@@ -0,0 +1,13 @@
+package com.blueharvest.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueHarvestBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueHarvestBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blueharvest/bank/BlueHarvestBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueharvest.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueHarvestBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
