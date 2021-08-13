de022cbed0a2ac7027a8f22ff9a2b895c9ba7450
Fathyelshemy8 <fathyelshemy8@gmail.com>
2021-08-14 01:16:40 +0200

    [bugfix] duplicates in saving subAccount

--- src/main/java/com/blueharvest/bank/services/SubAccountService.java
+++ src/main/java/com/blueharvest/bank/services/SubAccountService.java
@@ -5,9 +5,6 @@ import com.blueharvest.bank.dto.CustomerDto;
 import com.blueharvest.bank.dto.RequestAccountDto;
 import com.blueharvest.bank.dto.SubAccountDto;
 import com.blueharvest.bank.dto.TransactionDto;
-import com.blueharvest.bank.entities.SubAccount;
-import com.blueharvest.bank.repositories.SubAccountRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,13 +12,11 @@ import java.util.Optional;
 @Service
 public class SubAccountService {
 
-    private SubAccountRepository subAccountRepository;
 
     private TransactionService transactionService;
     private CustomerService customerService;
 
-    public SubAccountService(SubAccountRepository subAccountRepository, TransactionService transactionService,CustomerService customerService) {
-        this.subAccountRepository = subAccountRepository;
+    public SubAccountService(TransactionService transactionService,CustomerService customerService) {
         this.transactionService = transactionService;
         this.customerService=customerService;
     }
@@ -30,13 +25,9 @@ public class SubAccountService {
         CustomerDto parentDto= Optional.ofNullable(customerService.getCustomer(requestAccountDto.getCustomerID()))
                 .orElseThrow(()-> new ResourceNotFoundException("Can't find parent account",404));
 
-        ModelMapper modelMapper= new ModelMapper();
-
         SubAccountDto subAccountDto = new SubAccountDto();
         subAccountDto.setCustomer(parentDto);
         subAccountDto.setBalance(requestAccountDto.getInitialCredit());
-        SubAccount subAccount= modelMapper.map(subAccountDto,SubAccount.class);
-        subAccountRepository.save(subAccount);
         TransactionDto transactionDto=transactionService.buildTransactionObject(requestAccountDto.getInitialCredit(),parentDto,subAccountDto);
         transactionDto=transactionService.addTransaction(transactionDto);
         subAccountDto.setCustomer(transactionDto.getCustomer());
